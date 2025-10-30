846e02c8a7ad8624869a1afc82ffe16faacbd39c
Maximiliano <maxiarielmorales712@gmail.com>
2025-10-30 19:22:48 -0300

    capitulo 4

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
-import NavBar from './components/icons/NavBar.vue';
+import NavBar from './components/NavBar.vue';
+import DatosPersonales from './components/DatosPersonales.vue';
 </script>
 
 <template>
     <header>
         <!--Barra de Navegación-->
-      <NavBar />
-    </header>
+        <NavBar />
+        <!--Componente Datos Personales-->
+        <DatosPersonales />
+      
+      </header>
   <main>
     <section id="educacion"><h2>Educación - Cursos</h2></section>
     <section id="experiencia"><h2>Experiencia</h2></section>
---
+++ vue-project/src/components/DatosPersonales.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <section class="datos-personales">
+    <div class="card">
+      <h1>Maximiliano A Morales</h1>
+      <h2>Técnico Universitario en programacion - utn</h2>
+      <p>Hola, Bienvenido a mi portafolio de proyectos. Soy estudiante en programacion</p>
+      <ul class="container-lista">
+        <li v-for="red in redesSociales" :key="red.id">
+          <a :href="red.url"><img class="icon-redsocial" :src="red.src" width="35rem" :alt="red.name"></a>
+        </li>
+      </ul>
+      <h3>☎ Mi Teléfono personal: {+5492984661349</h3>
+      <h4>Gral. Roca, RN</h4>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
