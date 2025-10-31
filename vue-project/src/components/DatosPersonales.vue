155446ae6ff9989ae2f2009ce3c8a2052014e9c2
Maximiliano <maxiarielmorales712@gmail.com>
2025-10-30 22:12:49 -0300

    finalizado

--- vue-project/src/components/DatosPersonales.vue
+++ vue-project/src/components/DatosPersonales.vue
@@ -1,23 +1,83 @@
 <script setup>
+import linkedin from '/src/assets/linkedin_icon.svg';
+import instagram from '/src/assets/instagram_icon.svg';
+import github from '/src/assets/github_icon.svg';
+import cv from '/src/assets/cv_resume_icon.svg';
+
+const title = 'Maximiliano A Morales';
+const descripcion = 'Estudiante de Técnico Universitario en Programación - UTN';
+const residencia = 'Gral. Roca, RN, Argentina';
+const presentacion = 'Hola, Bienvenido a mi portafolio de proyectos. Soy un estudiante en programación.';
+const redesSociales = [
+  { id: 1, name: 'linkedin', src: linkedin, url: 'https://www.linkedin.com/in/maxi-morales-9b4541393/' },
+  { id: 2, name: 'Instagram', src: instagram, url: 'https://www.instagram.com/m.a.mor712/' },
+  { id: 3, name: 'github', src: github, url: 'https://github.com/MaxiMorales96' },
+  { id: 4, name: 'curriculum', src: cv, url: '' },
+];
+const telefono = '+54 9 2984-661349';
 </script>
 
 <template>
   <section class="datos-personales">
     <div class="card">
-      <h1>Maximiliano A Morales</h1>
-      <h2>Técnico Universitario en programacion - utn</h2>
-      <p>Hola, Bienvenido a mi portafolio de proyectos. Soy estudiante en programacion</p>
+      <h1>{{ title }}</h1>
+      <h2>{{ descripcion }}</h2>
+      <p>{{ presentacion }}</p>
       <ul class="container-lista">
         <li v-for="red in redesSociales" :key="red.id">
           <a :href="red.url"><img class="icon-redsocial" :src="red.src" width="35rem" :alt="red.name"></a>
         </li>
       </ul>
-      <h3>☎ Mi Teléfono personal: {+5492984661349</h3>
-      <h4>Gral. Roca, RN</h4>
+      <h3>☎ Mi Teléfono personal: {{ telefono }}</h3>
+      <h4>{{ residencia }}</h4>
     </div>
   </section>
 </template>
 
 <style scoped>
+h1 {
+  font-size: 2.5rem;
+}
 
+p {
+  font-size: 1.2rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+}
+
+.card {
+  background-color: rgb(28, 41, 52);
+  border-radius: 10px;
+  padding: 10px;
+  margin: 10px;
+  text-align: center;
+}
+
+.container-lista {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  text-align: center;
+}
+
+.icon-redsocial {
+  align-items: center;
+  background-color: aliceblue;
+  border-radius: 50%;
+  padding: 2px;
+  margin: 5px;
+  box-shadow: 0 0 5px rgba(95, 124, 205, 0.934);
+}
+
+.icon-redsocial:hover {
+  background-color: rgb(28, 41, 52);
+  box-shadow: 0 0 5px rgba(251, 249, 249, 0.934);
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 600;
+}
 </style>
---
+++ vue-project/src/components/InteresesComponente.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue';
+import FondoLava from './FondoLava.vue';
+const intereses = ref([
+    'Me encanta desarmar y reparar de todo; si es un celular, una computadora o cualquier aparato, me le animo. Si está roto, para mí es un desafío, que todo tiene solución.',
+]);
+</script>
+
+<template>
+    <div class="intereses-contenedor">
+        <FondoLava />
+        <ul class="contenedor-lista">
+            <li class="item" v-for="interes in intereses" :key="interes">
+                {{ interes }}
+            </li>
+        </ul>
+    </div>
+    <!-- <div class="intereses-contenedor">
+        <ul class="contenedor-lista">
+            <li class="item">
+                No me rindo fácil; para mí, cada cosa rota es un rompecabezas. Me encanta esa sensación de "revivir" la tecnología que otros ya dan por perdido.
+            </li>
+        </ul>
+    </div> -->
+</template>
+
+<style scoped>
+.intereses-contenedor {
+    display: flex;
+    position: relative;
+    margin: 0 auto;
+    max-width: 85%;
+    padding: 1rem;
+}
+
+.contenedor-lista {
+    list-style-type: none;
+    padding: 1rem;
+    margin-bottom: .5rem;
+    color: var(--vt-c-white-soft);
+    font-size: 1.4rem;
+    text-shadow: 3px 3px 6px rgba(0, 0, 0, 1);
+}
+
+.item {
+    margin-bottom: 1rem;
+}
+</style>
